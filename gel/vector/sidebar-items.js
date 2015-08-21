80aae491e719166e6b4ed5eeda3e505521244054
Cole Reynolds <cpjreynolds@gmail.com>
2015-08-21 01:50:17 +0000

    rebuild pages at 23525d4

--- gel/vector/sidebar-items.js
+++ gel/vector/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Vec2",""],["Vec3",""],["Vec4",""]]});
+initSidebarItems({"struct":[["Vec2",""],["Vec3",""],["Vec4",""]],"trait":[["Cross",""],["Dot",""],["Length",""],["Normalize",""],["Vector",""]]});
---
+++ implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['gel'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='gel/struct.Vec2.html' title='gel::Vec2'>Vec2</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='gel/struct.Vec3.html' title='gel::Vec3'>Vec3</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='gel/struct.Vec4.html' title='gel::Vec4'>Vec4</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
