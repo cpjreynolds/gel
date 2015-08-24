b988415915674634d70242d96f073e1785736934
Cole Reynolds <cpjreynolds@gmail.com>
2015-08-24 15:52:14 -0400

    more stuff

--- src/matrix.rs
+++ src/matrix.rs
@@ -3,6 +3,7 @@ use std::ops::{
     Add,
     Sub,
     Mul,
+    Div,
     Deref,
     DerefMut,
 };
@@ -20,18 +21,44 @@ use num::{
 use vector::{
     Vec3,
     Vec4,
+    Cross,
+    Dot,
+    Normalize,
 };
+
+pub trait Translate {
+    fn translation(v: Vec3) -> Self;
+    fn translate(&self, v: Vec3) -> Self;
+}
+
+pub trait Rotate {
+    fn rotation(angle: f32, axis: Vec3) -> Self;
+    fn rotate(&self, angle: f32, axis: Vec3) -> Self;
+}
+
+pub trait Inverse {
+    fn inverse(&self) -> Self;
+}
 
 pub trait Transpose {
     fn transpose(&self) -> Self;
 }
 
-pub trait Translate {
-    fn translation(v: Vec3) -> Self;
+pub trait Scale {
+    fn scaling(v: Vec3) -> Self;
+    fn scale(&self, v: Vec3) -> Self;
 }
 
-pub trait Rotate {
-    fn rotate(&self) -> Self;
+pub trait LookAt {
+    fn look_at(eye: Vec3, center: Vec3, up: Vec3) -> Self;
+}
+
+pub trait Projection {
+    fn perspective(fovy: f32, aspect: f32, znear: f32, zfar: f32) -> Self;
+}
+
+pub trait Matrix {
+    type Buffer;
 }
 
 #[repr(C)]
@@ -39,29 +66,31 @@ pub trait Rotate {
 pub struct Mat4(pub [Vec4; 4]);
 
 impl Mat4 {
-    pub fn get_row(&self, i: usize) -> Vec4 {
-        let mut result = Vec4::zero();
-        for j in 0..4 {
-            result[j] = self[j][i];
-        }
-        result
+    /// Create a new 4x4 matrix.
+    pub fn new(m11: f32, m12: f32, m13: f32, m14: f32,
+               m21: f32, m22: f32, m23: f32, m24: f32,
+               m31: f32, m32: f32, m33: f32, m34: f32,
+               m41: f32, m42: f32, m43: f32, m44: f32,) -> Mat4
+    {
+        Mat4([
+            Vec4 { x: m11, y: m21, z: m31, w: m41, },
+            Vec4 { x: m12, y: m22, z: m32, w: m42, },
+            Vec4 { x: m13, y: m23, z: m33, w: m43, },
+            Vec4 { x: m14, y: m24, z: m34, w: m44, },
+        ])
     }
+}
 
-    pub fn get_col(&self, i: usize) -> Vec4 {
-        let mut result = Vec4::zero();
-        for j in 0..4 {
-            result[j] = self[i][j];
-        }
-        result
-    }
+impl Matrix for Mat4 {
+    type Buffer = [Vec4; 4];
 }
 
 impl Add for Mat4 {
     type Output = Self;
 
-    fn add(self, other: Self) -> Self {
-        let mut result = Mat4::zero();
-        for i in 0..4 {
+    fn add(self, other: Self) -> Self::Output {
+        let mut result = Self::zero();
+        for i in 0..self.len() {
             result[i] = self[i] + other[i];
         }
         result
@@ -71,9 +100,9 @@ impl Add for Mat4 {
 impl Sub for Mat4 {
     type Output = Self;
 
-    fn sub(self, other: Self) -> Self {
-        let mut result = Mat4::zero();
-        for i in 0..4 {
+    fn sub(self, other: Self) -> Self::Output {
+        let mut result = Self::zero();
+        for i in 0..self.len() {
             result[i] = self[i] - other[i];
         }
         result
@@ -83,15 +112,17 @@ impl Sub for Mat4 {
 impl Mul for Mat4 {
     type Output = Self;
 
-    fn mul(self, other: Self) -> Self {
-        let mut result = Mat4::zero();
-        for col in 0..4 {
-            for row in 0..4 {
-                let mut sum = 0.0;
-                for i in 0..4 {
-                    sum += self[col][i] * other[i][row];
+    fn mul(self, other: Self) -> Self::Output {
+        let mut result = Self::zero();
+
+        for i in 0..self.len() {
+            for j in 0..self.len() {
+                let mut acc = f32::zero();
+
+                for k in 0..self.len() {
+                    acc += self[i][k] * other[k][j];
                 }
-                result[col][row] = sum;
+                result[i][j] = acc;
             }
         }
         result
@@ -103,10 +134,34 @@ impl Mul<Vec4> for Mat4 {
 
     fn mul(self, other: Vec4) -> Vec4 {
         let mut result = Vec4::zero();
-        for j in 0..4 {
-            for i in 0..4 {
-                result[j] += self[i][j] * other[i];
+        for i in 0..self.len() {
+            for j in 0..self.len() {
+                result[i] += self[i][j] * other[j];
             }
+        }
+        result
+    }
+}
+
+impl Add<f32> for Mat4 {
+    type Output = Self;
+
+    fn add(self, other: f32) -> Self::Output {
+        let mut result = Self::zero();
+        for i in 0..self.len() {
+            result[i] = self[i] + other;
+        }
+        result
+    }
+}
+
+impl Sub<f32> for Mat4 {
+    type Output = Self;
+
+    fn sub(self, other: f32) -> Self::Output {
+        let mut result = Self::zero();
+        for i in 0..self.len() {
+            result[i] = self[i] - other;
         }
         result
     }
@@ -115,64 +170,59 @@ impl Mul<Vec4> for Mat4 {
 impl Mul<f32> for Mat4 {
     type Output = Self;
 
-    fn mul(self, other: f32) -> Self {
-        let mut result = Mat4::zero();
-        for i in 0..4 {
+    fn mul(self, other: f32) -> Self::Output {
+        let mut result = Self::zero();
+        for i in 0..self.len() {
             result[i] = self[i] * other;
         }
         result
     }
 }
 
+impl Div<f32> for Mat4 {
+    type Output = Self;
+
+    fn div(self, other: f32) -> Self::Output {
+        let mut result = Self::zero();
+        for i in 0..self.len() {
+            result[i] = self[i] / other;
+        }
+        result
+    }
+}
+
+impl Inverse for Mat4 {
+    fn inverse(&self) -> Self {
+        unimplemented!()
+    }
+}
+
 impl Zero for Mat4 {
     fn zero() -> Self {
-        Mat4([Vec4::zero(); 4])
+        Self::from([Vec4::zero(); 4])
     }
 
     fn is_zero(&self) -> bool {
-        *self == Mat4::zero()
+        *self == Self::zero()
     }
 }
 
 impl One for Mat4 {
     fn one() -> Self {
-        let mut result = Mat4::zero();
-        result[0][0] = 1.0;
-        result[1][1] = 1.0;
-        result[2][2] = 1.0;
-        result[3][3] = 1.0;
-        result
-    }
-}
-
-impl AsRef<[Vec4; 4]> for Mat4 {
-    fn as_ref<'a>(&'a self) -> &'a [Vec4; 4] {
-        unsafe {
-            mem::transmute(self)
+        let mut result = Self::zero();
+        for i in 0..4 {
+            result[i][i] = f32::one();
         }
-    }
-}
-
-impl Deref for Mat4 {
-    type Target = [Vec4; 4];
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for Mat4 {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
+        result
     }
 }
 
 impl Transpose for Mat4 {
     fn transpose(&self) -> Self {
-        let mut result = Mat4::zero();
-        for j in 0..4 {
-            for i in 0..4 {
-                result[i][j] = self[j][i];
+        let mut result = Self::zero();
+        for i in 0..self.len() {
+            for j in 0..self.len() {
+                result[j][i] = self[i][j];
             }
         }
         result
@@ -187,6 +237,111 @@ impl Translate for Mat4 {
         result[3][2] = v[2];
         result
     }
+
+    fn translate(&self, v: Vec3) -> Self {
+        let mut result = self.clone();
+        result[3] = self[0] * v[0] + self[1] * v[1] + self[2] * v[2] + self[3];
+        result
+    }
+}
+
+impl Rotate for Mat4 {
+    fn rotation(angle: f32, axis: Vec3) -> Self {
+        let a = angle;
+        let c = a.cos();
+        let s = a.sin();
+
+        let axis = axis.normalize();
+
+        let mut result = Self::one();
+
+        result[0][0] = c + (1.0 - c) * axis.x * axis.x;
+        result[0][1] = (1.0 - c) * axis.x * axis.y + s * axis.z;
+        result[0][2] = (1.0 - c) * axis.x * axis.z - s * axis.y;
+        result[0][3] = 0.0;
+
+        result[1][0] = (1.0 - c) * axis.y * axis.x - s * axis.z;
+        result[1][1] = c + (1.0 - c) * axis.y * axis.y;
+        result[1][2] = (1.0 - c) * axis.y * axis.z + s * axis.x;
+        result[1][3] = 0.0;
+
+        result[2][0] = (1.0 - c) * axis.z * axis.x + s * axis.y;
+        result[2][1] = (1.0 - c) * axis.z * axis.y - s * axis.x;
+        result[2][2] = c + (1.0 - c) * axis.z * axis.z;
+        result[2][3] = 0.0;
+
+        result[3] = Vec4::new(0.0, 0.0, 0.0, 1.0);
+
+        result
+    }
+
+    fn rotate(&self, angle: f32, axis: Vec3) -> Self {
+        let r = Self::rotation(angle, axis);
+        *self * r
+    }
+}
+
+impl Projection for Mat4 {
+    fn perspective(fovy: f32, aspect: f32, znear: f32, zfar: f32) -> Self {
+        let tan_half_fovy = (fovy / 2.0).tan();
+        let mut result = Self::zero();
+
+        result[0][0] = 1.0 / (aspect * tan_half_fovy);
+        result[1][1] = 1.0 / (tan_half_fovy);
+        result[2][2] = (zfar + znear) / (zfar - znear);
+        result[2][3] = 1.0;
+        result[3][2] = -(2.0 * zfar * znear) / (zfar - znear);
+
+        result
+    }
+}
+
+impl Scale for Mat4 {
+    fn scaling(v: Vec3) -> Self {
+        let mut result = Self::one();
+
+        result[3][0] = v.x;
+        result[3][1] = v.y;
+        result[3][2] = v.z;
+
+        result
+    }
+
+    fn scale(&self, v: Vec3) -> Self {
+        let mut result = Self::one();
+
+        result[0] = self[0] * v[0];
+        result[1] = self[1] * v[1];
+        result[2] = self[2] * v[2];
+        result[3] = self[3];
+
+        result
+    }
+}
+
+impl LookAt for Mat4 {
+    fn look_at(eye: Vec3, center: Vec3, up: Vec3) -> Self {
+        let f = (center - eye).normalize();
+        let s = f.cross(&up).normalize();
+        let u = s.cross(&f);
+
+        let mut result = Self::one();
+
+        result[0][0] = s.x;
+        result[1][0] = s.y;
+        result[2][0] = s.z;
+        result[0][1] = u.x;
+        result[1][1] = u.y;
+        result[2][1] = u.z;
+        result[0][2] = -f.x;
+        result[1][2] = -f.y;
+        result[2][2] = -f.z;
+        result[3][0] = -(s.dot(&eye));
+        result[3][1] = -(u.dot(&eye));
+        result[3][2] = f.dot(&eye);
+
+        result
+    }
 }
 
 impl AsUniformValue for Mat4 {
@@ -195,5 +350,117 @@ impl AsUniformValue for Mat4 {
             mem::transmute(*self)
         })
     }
+}
 
+impl From<[Vec4; 4]> for Mat4 {
+    fn from(ary: [Vec4; 4]) -> Self {
+        unsafe {
+            mem::transmute(ary)
+        }
+    }
+}
+
+impl Into<[Vec4; 4]> for Mat4 {
+    fn into(self) -> [Vec4; 4] {
+        unsafe {
+            mem::transmute(self)
+        }
+    }
+}
+
+impl AsRef<[Vec4; 4]> for Mat4 {
+    fn as_ref<'a>(&'a self) -> &'a [Vec4; 4] {
+        unsafe {
+            mem::transmute(self)
+        }
+    }
+}
+
+impl AsMut<[Vec4; 4]> for Mat4 {
+    fn as_mut<'a>(&'a mut self) -> &'a mut [Vec4; 4] {
+        unsafe {
+            mem::transmute(self)
+        }
+    }
+}
+
+impl Deref for Mat4 {
+    type Target = <Self as Matrix>::Buffer;
+
+    fn deref<'a>(&'a self) -> &'a Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Mat4 {
+    fn deref_mut<'a>(&'a mut self) -> &'a mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use num::{
+        Zero,
+        One,
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_matrix_new() {
+        let mat = Mat4::new(1.0, 0.0, 1.0, 0.0,
+                            0.0, 1.0, 0.0, 1.0,
+                            1.0, 0.0, 1.0, 0.0,
+                            0.0, 1.0, 0.0, 1.0,);
+        for i in 0..4 {
+            for j in 0..4 {
+                if (i + j) % 2 == 0 {
+                    assert!(mat[i][j] == 1.0);
+                } else {
+                    assert!(mat[i][j] == 0.0);
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_matrix_transpose() {
+        let mat1 = Mat4::new(1.0, 0.0, 0.0, 0.0,
+                             1.0, 1.0, 0.0, 0.0,
+                             1.0, 1.0, 1.0, 0.0,
+                             1.0, 1.0, 1.0, 1.0,);
+
+        let mat2 = Mat4::new(1.0, 1.0, 1.0, 1.0,
+                             0.0, 1.0, 1.0, 1.0,
+                             0.0, 0.0, 1.0, 1.0,
+                             0.0, 0.0, 0.0, 1.0,);
+
+        assert!(mat1 == mat2.transpose());
+    }
+
+    #[test]
+    fn test_matrix_zero() {
+        let mat = Mat4::zero();
+        for i in 0..4 {
+            for j in 0..4 {
+                assert!(mat[i][j] == 0.0);
+            }
+        }
+    }
+
+    #[test]
+    fn test_matrix_one() {
+        let mat = Mat4::one();
+
+        for i in 0..4 {
+            for j in 0..4 {
+                if i == j {
+                    assert!(mat[i][j] == 1.0);
+                } else {
+                    assert!(mat[i][j] == 0.0);
+                }
+            }
+        }
+    }
 }
--- src/vector.rs
+++ src/vector.rs
@@ -3,6 +3,7 @@ use std::ops::{
     Sub,
     Mul,
     Div,
+    Neg,
     Deref,
     DerefMut,
 };
@@ -17,7 +18,7 @@ use glium::uniforms::{
     UniformValue,
 };
 
-pub trait Vector: Sized + Copy 
+pub trait Vector: Sized + Copy
     + Dot + Normalize + Length
     + Add<Output=Self> + Add<f32, Output=Self>
     + Sub<Output=Self> + Sub<f32, Output=Self>
@@ -27,11 +28,11 @@ pub trait Vector: Sized + Copy
 }
 
 pub trait Dot {
-    fn dot(&self, other: Self) -> f32;
+    fn dot(&self, other: &Self) -> f32;
 }
 
 pub trait Cross {
-    fn cross(&self, other: Self) -> Self;
+    fn cross(&self, other: &Self) -> Self;
 }
 
 pub trait Normalize {
@@ -46,7 +47,7 @@ impl<T> Length for T
     where T: Dot + Copy
 {
     fn length(&self) -> f32 {
-        (self.dot(*self)).sqrt()
+        self.dot(self).sqrt()
     }
 }
 
@@ -59,12 +60,157 @@ impl<T> Normalize for T
     }
 }
 
+macro_rules! vec_binop_vec_impl {
+    ($imp:ident, $method:ident, $t:ty) => {
+        impl $imp for $t {
+            type Output = Self;
+
+            fn $method(self, other: Self) -> Self::Output {
+                let mut result = Self::zero();
+                for i in 0..self.len() {
+                    result[i] = self[i].$method(other[i]);
+                }
+                result
+            }
+        }
+    }
+}
+
+macro_rules! vec_binop_scalar_impl {
+    ($imp:ident, $method:ident, $t:ty) => {
+        impl $imp<f32> for $t {
+            type Output = Self;
+
+            fn $method(self, other: f32) -> Self::Output {
+                let mut result = Self::zero();
+                for i in 0..self.len() {
+                    result[i] = self[i].$method(other)
+                }
+                result
+            }
+        }
+    }
+}
+
+macro_rules! vec_neg_impl {
+    ($t:ty) => {
+        impl Neg for $t {
+            type Output = Self;
+
+            fn neg(self) -> Self::Output {
+                let mut result = Self::zero();
+                for i in 0..self.len() {
+                    result[i] = -self[i];
+                }
+                result
+            }
+        }
+    }
+}
+
+macro_rules! vec_dot_impl {
+    ($t:ty) => {
+        impl Dot for $t {
+            fn dot(&self, other: &Self) -> f32 {
+                let mut result = f32::zero();
+                for i in 0..self.len() {
+                    result += self[i] * other[i];
+                }
+                result
+            }
+        }
+    }
+}
+
+macro_rules! vec_as_ref_mut_impl {
+    ($t:ty, $r:ty) => {
+        impl AsRef<$r> for $t {
+            fn as_ref(&self) -> &$r {
+                unsafe {
+                    mem::transmute(self)
+                }
+            }
+        }
+
+        impl AsMut<$r> for $t {
+            fn as_mut(&mut self) -> &mut $r {
+                unsafe {
+                    mem::transmute(self)
+                }
+            }
+        }
+    }
+}
+
+macro_rules! vec_from_into_impl {
+    ($t:ty, $r:ty) => {
+        impl From<$r> for $t {
+            fn from(ary: $r) -> Self {
+                unsafe {
+                    mem::transmute(ary)
+                }
+            }
+        }
+
+        impl Into<$r> for $t {
+            fn into(self) -> $r {
+                unsafe {
+                    mem::transmute(self)
+                }
+            }
+        }
+    }
+}
+
+macro_rules! vec_deref_impl {
+    ($t:ty, $r:ty) => {
+        impl Deref for $t {
+            type Target = $r;
+
+            fn deref(&self) -> &Self::Target {
+                unsafe {
+                    mem::transmute(self)
+                }
+            }
+        }
+
+        impl DerefMut for $t {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                unsafe {
+                    mem::transmute(self)
+                }
+            }
+        }
+    }
+}
+
+macro_rules! vec_impl {
+    ($t:ty, $buf:ty) => {
+        vec_binop_vec_impl!(Add, add, $t);
+        vec_binop_vec_impl!(Sub, sub, $t);
+
+        vec_binop_scalar_impl!(Add, add, $t);
+        vec_binop_scalar_impl!(Sub, sub, $t);
+        vec_binop_scalar_impl!(Mul, mul, $t);
+        vec_binop_scalar_impl!(Div, div, $t);
+
+        vec_neg_impl!($t);
+
+        vec_dot_impl!($t);
+
+        vec_as_ref_mut_impl!($t, $buf);
+        vec_from_into_impl!($t, $buf);
+        vec_deref_impl!($t, $buf);
+    }
+}
+
 #[repr(C)]
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Vec2 {
     pub x: f32,
     pub y: f32,
 }
+vec_impl!(Vec2, [f32; 2]);
 
 impl Vec2 {
     pub fn new(x: f32, y: f32) -> Vec2 {
@@ -73,92 +219,14 @@ impl Vec2 {
             y: y,
         }
     }
+
+    pub fn extend(&self, z: f32) -> Vec3 {
+        Vec3::new(self.x, self.y, z)
+    }
 }
 
 impl Vector for Vec2 {
     type Buffer = [f32; 2];
-}
-
-impl Add for Vec2 {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] + other[i];
-        }
-        result
-    }
-}
-
-impl Add<f32> for Vec2 {
-    type Output = Self;
-
-    fn add(self, other: f32) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] + other;
-        }
-        result
-    }
-}
-
-impl Sub for Vec2 {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] - other[i];
-        }
-        result
-    }
-}
-
-impl Sub<f32> for Vec2 {
-    type Output = Self;
-
-    fn sub(self, other: f32) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] - other;
-        }
-        result
-    }
-}
-
-impl Mul<f32> for Vec2 {
-    type Output = Self;
-
-    fn mul(self, other: f32) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] * other;
-        }
-        result
-    }
-}
-
-impl Div<f32> for Vec2 {
-    type Output = Self;
-
-    fn div(self, other: f32) -> Self {
-         let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] + other;
-        }
-        result
-    }
-}
-
-impl Dot for Vec2 {
-    fn dot(&self, other: Self) -> f32 {
-        let mut result = f32::zero();
-        for i in 0..self.len() {
-            result += self[i] * other[i];
-        }
-        result
-    }
 }
 
 impl Zero for Vec2 {
@@ -168,40 +236,6 @@ impl Zero for Vec2 {
 
     fn is_zero(&self) -> bool {
         *self == Self::zero()
-    }
-}
-
-impl AsRef<[f32; 2]> for Vec2 {
-    fn as_ref<'a>(&'a self) -> &'a [f32; 2] {
-        unsafe {
-            mem::transmute(self)
-        }
-    }
-}
-
-impl From<[f32; 2]> for Vec2 {
-    fn from(ary: [f32; 2]) -> Self {
-        unsafe {
-            mem::transmute(ary)
-        }
-    }
-}
-
-impl Deref for Vec2 {
-    type Target = <Self as Vector>::Buffer;
-
-    fn deref<'a>(&'a self) -> &'a Self::Target {
-        unsafe {
-            mem::transmute(self)
-        }
-    }
-}
-
-impl DerefMut for Vec2 {
-    fn deref_mut<'a>(&'a mut self) -> &'a mut Self::Target {
-        unsafe {
-            mem::transmute(self)
-        }
     }
 }
 
@@ -220,6 +254,7 @@ pub struct Vec3 {
     pub y: f32,
     pub z: f32,
 }
+vec_impl!(Vec3, [f32; 3]);
 
 impl Vec3 {
     pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
@@ -229,96 +264,22 @@ impl Vec3 {
             z: z,
         }
     }
+
+    pub fn truncate(&self) -> Vec2 {
+        Vec2::new(self.x, self.y)
+    }
+
+    pub fn extend(&self, w: f32) -> Vec4 {
+        Vec4::new(self.x, self.y, self.z, w)
+    }
 }
 
 impl Vector for Vec3 {
     type Buffer = [f32; 3];
 }
 
-impl Add for Vec3 {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] + other[i];
-        }
-        result
-    }
-}
-
-impl Add<f32> for Vec3 {
-    type Output = Self;
-
-    fn add(self, other: f32) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] + other;
-        }
-        result
-    }
-}
-
-impl Sub for Vec3 {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] - other[i];
-        }
-        result
-    }
-}
-
-impl Sub<f32> for Vec3 {
-    type Output = Self;
-
-    fn sub(self, other: f32) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] - other;
-        }
-        result
-    }
-}
-
-impl Mul<f32> for Vec3 {
-    type Output = Self;
-
-    fn mul(self, other: f32) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] * other;
-        }
-        result
-    }
-}
-
-impl Div<f32> for Vec3 {
-    type Output = Self;
-
-    fn div(self, other: f32) -> Self {
-         let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] + other;
-        }
-        result
-    }
-}
-
-impl Dot for Vec3 {
-    fn dot(&self, other: Self) -> f32 {
-        let mut result = f32::zero();
-        for i in 0..self.len() {
-            result += self[i] * other[i];
-        }
-        result
-    }
-}
-
 impl Cross for Vec3 {
-    fn cross(&self, other: Self) -> Self {
+    fn cross(&self, other: &Self) -> Self {
         let mut result = Vec3::zero();
         result[0] = self[1] * other[2] - self[2] * other[1];
         result[1] = self[2] * other[0] - self[0] * other[2];
@@ -334,40 +295,6 @@ impl Zero for Vec3 {
 
     fn is_zero(&self) -> bool {
         *self == Self::zero()
-    }
-}
-
-impl AsRef<[f32; 3]> for Vec3 {
-    fn as_ref<'a>(&'a self) -> &'a [f32; 3] {
-        unsafe {
-            mem::transmute(self)
-        }
-    }
-}
-
-impl From<[f32; 3]> for Vec3 {
-    fn from(ary: [f32; 3]) -> Self {
-        unsafe {
-            mem::transmute(ary)
-        }
-    }
-}
-
-impl Deref for Vec3 {
-    type Target = <Self as Vector>::Buffer;
-
-    fn deref<'a>(&'a self) -> &'a Self::Target {
-        unsafe {
-            mem::transmute(self)
-        }
-    }
-}
-
-impl DerefMut for Vec3 {
-    fn deref_mut<'a>(&'a mut self) -> &'a mut Self::Target {
-        unsafe {
-            mem::transmute(self)
-        }
     }
 }
 
@@ -387,6 +314,7 @@ pub struct Vec4 {
     pub z: f32,
     pub w: f32,
 }
+vec_impl!(Vec4, [f32; 4]);
 
 impl Vec4 {
     pub fn new(x: f32, y: f32, z: f32, w: f32) -> Vec4 {
@@ -397,92 +325,14 @@ impl Vec4 {
             w: w,
         }
     }
+
+    pub fn truncate(&self) -> Vec3 {
+        Vec3::new(self.x, self.y, self.z)
+    }
 }
 
 impl Vector for Vec4 {
     type Buffer = [f32; 4];
-}
-
-impl Add for Vec4 {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] + other[i];
-        }
-        result
-    }
-}
-
-impl Add<f32> for Vec4 {
-    type Output = Self;
-
-    fn add(self, other: f32) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] + other;
-        }
-        result
-    }
-}
-
-impl Sub for Vec4 {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] - other[i];
-        }
-        result
-    }
-}
-
-impl Sub<f32> for Vec4 {
-    type Output = Self;
-
-    fn sub(self, other: f32) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] - other;
-        }
-        result
-    }
-}
-
-impl Mul<f32> for Vec4 {
-    type Output = Self;
-
-    fn mul(self, other: f32) -> Self {
-        let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] * other;
-        }
-        result
-    }
-}
-
-impl Div<f32> for Vec4 {
-    type Output = Self;
-
-    fn div(self, other: f32) -> Self {
-         let mut result = Self::zero();
-        for i in 0..self.len() {
-            result[i] = self[i] + other;
-        }
-        result
-    }
-}
-
-impl Dot for Vec4 {
-    fn dot(&self, other: Self) -> f32 {
-        let mut result = f32::zero();
-        for i in 0..self.len() {
-            result += self[i] * other[i];
-        }
-        result
-    }
 }
 
 impl Zero for Vec4 {
@@ -492,40 +342,6 @@ impl Zero for Vec4 {
 
     fn is_zero(&self) -> bool {
         *self == Self::zero()
-    }
-}
-
-impl AsRef<[f32; 4]> for Vec4 {
-    fn as_ref<'a>(&'a self) -> &'a [f32; 4] {
-        unsafe {
-            mem::transmute(self)
-        }
-    }
-}
-
-impl From<[f32; 4]> for Vec4 {
-    fn from(ary: [f32; 4]) -> Self {
-        unsafe {
-            mem::transmute(ary)
-        }
-    }
-}
-
-impl Deref for Vec4 {
-    type Target = <Self as Vector>::Buffer;
-
-    fn deref<'a>(&'a self) -> &'a Self::Target {
-        unsafe {
-            mem::transmute(self)
-        }
-    }
-}
-
-impl DerefMut for Vec4 {
-    fn deref_mut<'a>(&'a mut self) -> &'a mut Self::Target {
-        unsafe {
-            mem::transmute(self)
-        }
     }
 }
 
